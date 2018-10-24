15cfeaf90cc5ed48a42804e170fb4fbec01243d7
Sylvain Junca <sylvain.junca@gmail.com>
2018-10-24 17:08:28 +0000

    creating the object variable and the for loop structure

--- character-counting.js
+++ character-counting.js
@@ -1,5 +1,9 @@
 function countLetters(string) {
   var newString = string.replace(/\s+/g, ''); //removes the spaces. 
+  var count = {};
+  for(let i =0; i < newString.length; i++) { //For loop to iterate each character
+    
+  }
   return newString;
 }
 
