a1c80c40c9deeee93f42f1b1f8a8fa70c1299914
Sylvain Junca <sylvain.junca@gmail.com>
2018-10-24 17:19:50 +0000

    creating the line to create the parameter in the objects and count letter

--- character-counting.js
+++ character-counting.js
@@ -2,9 +2,9 @@ function countLetters(string) {
   var newString = string.replace(/\s+/g, ''); //removes the spaces. 
   var count = {};
   for(let i =0; i < newString.length; i++) { //For loop to iterate each character
-    
+    count[newString[i]] = (count[newString[i]] || 0) + 1; // check if the parameter exists or not . Assign 0 or adds 1
   }
-  return newString;
+  return count;
 }
 
 console.log(countLetters('salut les amis'));
